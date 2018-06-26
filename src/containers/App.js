80ce62618abaee1b2d1cd8a70982070bedbee359
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-06-26 23:23:00 +0200

    Task 16.7

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
 import Title from '../components/Title';
+import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
     constructor(props){
@@ -32,4 +33,4 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+export default hot(module)(App);
